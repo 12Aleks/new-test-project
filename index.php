a17b105b5372631258e3ce3ef7d61466c1ba634f
Aleks-laptop\Алексей <otpusk.kiev@gmail.com>
2017-08-26 16:23:44 +0200

    new fails

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<heder>
+    <title>My php code</title>
+    <link  rel="stylesheet" type="text/css" href="style.css">
+</heder>
+<body>
+<div class="body">
+<form >
+    Введите ваш логин: <input type="login"><br><br>
+    Введите ваш пароль: <input type="password"><br><br>
+    Введите ваш email:<input type="email"><br><br>
+    <input type="submit">
+</form>
+</div>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 26.08.2017
+ * Time: 16:10
+ */
