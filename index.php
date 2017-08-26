51a6314d44179a1b57d564159c4795ed3e2af35c
Aleks-laptop\Алексей <otpusk.kiev@gmail.com>
2017-08-26 21:31:50 +0200

    new code css

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
 <body>
 <div class="body">
 <form class="form" action="/new/new-test-project/model.php" method="post" >
+    <p>Если вы зарегестрированы: </p>
     Введите ваш логин: <input type="login" name="login"><br><br>
     Введите ваш пароль: <input type="password" name="pas"><br><br>
     Введите ваш email:<input type="email" name="mail"><br><br>
     <input type="submit" class="input">
 </form>
+</div>
+<div class="body1">
+    <form class="form" action="/new/new-test-project/model.php" method="post" >
+        <p>Если вы не зарегестрированы: </p>
+        Введите ваш логин: <input type="login" name="new_login"><br><br>
+        Введите ваш пароль: <input type="password" name="new_pas"><br><br>
+        Введите ваш email:<input type="email" name="mail"><br><br>
+        <input type="submit" class="input1">
+    </form>
 </div>
 </body>
 </html>
