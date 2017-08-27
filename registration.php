1d66ab1600a859dec2c401d68d15da708b848733
Aleks-laptop\Алексей <otpusk.kiev@gmail.com>
2017-08-27 18:15:37 +0200

    new form registration

--- registration.php
+++ registration.php
@@ -9,17 +9,15 @@ $conn = new mysqli('localhost', 'Aleks', 'af7rwbh6dFxkzHYK', 'test');
 if($conn->connect_error){
     echo 'Error connect it server!';
 }
-$dbase = "SELECT id FROM mytable WHERE email != '$mail' ";
+$dbase = "SELECT id FROM mytable WHERE email='$mail'";
 $rez = $conn->query($dbase);
-if($con = $rez->num_rows > 0) {
-
-    $new_data = "INSERT INTO mytable (login, password, email) VALUES('$login', '$pas', '$mail')";
-    if ($conn->query($new_data) === FALSE) {
-        echo '<p class="text1">Sorry, data is not added</p>';
-    }else{
-        echo '<p class="text">Поздравляем, вы зарегестрировались!</p>';
-        echo "<br><br><a href='index.php'>"."<input class='input1' value='Перейти к авторизации'>". "</a>";
-    }
+ if($rez->num_rows > 0) {
+   die('<p class="text">' . 'Пользователь с почтовым адресом'. ' ' . '<b class="text1">' . $mail . '</b>' . ' ' .'уже существует!!!'. '</p>');
 }else{
-    echo '<p class="text1">Пользователь с таким email уже сущесвует!</p>';
-}
+$new_data = "INSERT INTO mytable (login, password, email) VALUES('$login', '$pas', '$mail')";
+if ($conn->query($new_data) === FALSE) {
+    echo '<p class="text1">Sorry, data is not added</p>';
+}else{
+    echo '<p class="text">Поздравляем, вы зарегестрировались!</p>';
+    echo "<a href='index.php'>"."<input class='input1' value='Перейти к авторизации'>". "</a>";
+}}
